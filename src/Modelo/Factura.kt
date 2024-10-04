ded81eb391d00e52825d9917986a4753f399e83a
fatimaMontano <fatima.montano16@gmail.com>
2024-10-04 17:50:55 -0600

    new changes

--- src/Modelo/Factura.kt
+++ src/Modelo/Factura.kt
@@ -1,7 +1,10 @@
 package Modelo
 
 data class Factura(
-    val items: List<CarritoItem>,
-    val total: Double,
+    val items: List<CarritoItem>,      // Lista de productos en la factura
+    val subtotal: Double,              // Suma del precio total de los productos sin impuestos
+    val porcentajeImpuesto: Double,    // El porcentaje de impuestos (por ejemplo, 0.13 para el 13%)
+    val impuestos: Double,             // Valor calculado de los impuestos
+    val total: Double,                 // Total general incluyendo impuestos
     val fecha: String
 )
